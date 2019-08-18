9c03a2066672fb1328e6ea4000b0b7cbf90cca7a
Kem Crimmins <kem.crimmins@gmail.com>
2019-08-18 09:59:46 -0400

    numbers in ruby

---
+++ introToProgramming/numbers_lecture.rb
@@ -0,0 +1,24 @@
+# numbers in ruby
+
+puts 1 + 1
+puts -5 + 3
+puts 2.5 + 0.5
+
+puts 5 / 2 # prints 2
+puts 5 / 2.0 # prints 2.5
+
+puts 13 % 5 # prints 3
+
+# Ruby number comparisons
+#
+# equal ==
+# not equal !=
+# greater than >
+# less than <
+# greater than or equal to >=
+# less than or equal to <=
+
+puts 5 == 5 # true
+puts 5 < 5 # false
+
+
