f45729a122773b6d4239549a924ff5e874afaaf4
Kem Crimmins <kem.crimmins@gmail.com>
2019-08-27 16:58:18 -0400

    enumerables in ruby

--- introToProgramming/enumerables_lecture.rb
+++ introToProgramming/enumerables_lecture.rb
@@ -2,11 +2,25 @@
 
 months = ["Jan", "Feb", "Mar", "Apr"]
 
-i = 0
+i = 0 
 while i < months.length
   month = months[i]
   puts month
 
   i += 1
+end
+
+months.each { |month| puts month }
+
+puts "do block"
+months.each do |month|
+  puts month
+  puts "---"
+end
+
+months.each_with_index do |month, index|
+  puts month
+  puts index
+  puts "___"
 end
 
